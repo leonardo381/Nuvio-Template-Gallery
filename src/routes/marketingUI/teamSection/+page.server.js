2a638c76a5162de17b3e31e41afa889e4ca5230b
leonardo381 <58829408+leonardo381@users.noreply.github.com>
2026-04-06 22:33:09 +0100

    Add team section

---
+++ src/routes/marketingUI/teamSection/+page.server.js
@@ -0,0 +1,16 @@
+import {
+  getWebsiteBySlug,
+  getPageBySlug,
+  getBlocksByPageId,
+  mapBlocksBySlot
+} from '$lib/server/content';
+
+export async function load({ locals }) {
+  const website = await getWebsiteBySlug(locals.pb, 'demo-site');
+  const page = await getPageBySlug(locals.pb, website.id, 'teamSection');
+  const blocks = await getBlocksByPageId(locals.pb, page.id);
+
+  return {
+    blocksBySlot: mapBlocksBySlot(blocks)
+  };
+}
---
+++ src/routes/marketingUI/teamSection/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+  import TeamSection from '$lib/components/marktingUI/teamSection.svelte';
+  export let data;
+
+  const blocks = data.blocksBySlot ?? {};
+
+  const teamSectionDefault = blocks['teamSection.teamSectionDefault']?.props;
+  const teamSectionClean = blocks['teamSection.teamSectionClean']?.props;
+  const teamSectionDescription = blocks['teamSection.teamSectionDescription']?.props;
+  const teamSectionCarousel = blocks['teamSection.teamSectionCarousel']?.props;
+  const teamSectionCard = blocks['teamSection.teamSectionCard']?.props;
+  const teamSectionOverlay = blocks['teamSection.teamSectionOverlay']?.props;
+  const teamSectionCTA = blocks['teamSection.teamSectionCTA']?.props;
+  const teamSectionOverlayCTA = blocks['teamSection.teamSectionOverlayCTA']?.props;
+</script>
+
+{#if teamSectionDefault}
+  <TeamSection variant="teamSectionDefault" data={teamSectionDefault} />
+{/if}
+
+{#if teamSectionClean}
+  <TeamSection variant="teamSectionClean" data={teamSectionClean} />
+{/if}
+
+{#if teamSectionDescription}
+  <TeamSection variant="teamSectionDescription" data={teamSectionDescription} />
+{/if}
+
+{#if teamSectionCarousel}
+  <TeamSection variant="teamSectionCarousel" data={teamSectionCarousel} />
+{/if}
+
+{#if teamSectionCard}
+  <TeamSection variant="teamSectionCard" data={teamSectionCard} />
+{/if}
+
+{#if teamSectionOverlay}
+  <TeamSection variant="teamSectionOverlay" data={teamSectionOverlay} />
+{/if}
+
+{#if teamSectionCTA}
+  <TeamSection variant="teamSectionCTA" data={teamSectionCTA} />
+{/if}
+
+{#if teamSectionOverlayCTA}
+  <TeamSection variant="teamSectionOverlayCTA" data={teamSectionOverlayCTA} />
+{/if}
