1502cd84ba22759a3c1ca8faebf6dcc80cb6bbc9
leonardo381 <58829408+leonardo381@users.noreply.github.com>
2026-04-07 19:51:17 +0100

    add testimonial

---
+++ src/routes/marketingUI/testimonial/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Testimonial from '$lib/components/marktingUI/testimonial.svelte';
+  export let data;
+
+  const blocks = data.blocksBySlot ?? {};
+
+  const testimonialBlockquote = blocks['testimonial.testimonialBlockquote']?.props;
+  const testimonialCards = blocks['testimonial.testimonialCards']?.props;
+  const testimonialTabs = blocks['testimonial.testimonialTabs']?.props;
+  const testimonialCarousel = blocks['testimonial.testimonialCarousel']?.props;
+  const testimonialGrid = blocks['testimonial.testimonialGrid']?.props;
+</script>
+
+{#if testimonialBlockquote}
+  <Testimonial variant="testimonialBlockquote" data={testimonialBlockquote} />
+{/if}
+{#if testimonialCards}
+  <Testimonial variant="testimonialCards" data={testimonialCards} />
+{/if}
+{#if testimonialTabs}
+  <Testimonial variant="testimonialTabs" data={testimonialTabs} />
+{/if}
+{#if testimonialCarousel}
+  <Testimonial variant="testimonialCarousel" data={testimonialCarousel} />
+{/if}
+{#if testimonialGrid}
+  <Testimonial variant="testimonialGrid" data={testimonialGrid} />
+{/if}
