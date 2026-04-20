4833b763e84e0070e8a547bb07c04dbd065a030c
leonardo381 <58829408+leonardo381@users.noreply.github.com>
2026-04-20 20:40:29 +0100

    Add feature routes

---
+++ src/routes/features/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Features from '$lib/components/coreUI/features.svelte';
+  export let data;
+
+  const blocks = data.blocksBySlot ?? {};
+
+  const featureWhatsApp = blocks['feature.featureWhatsApp']?.props;
+  const featureContact = blocks['feature.Contact']?.props;
+
+</script>
+
+{#if featureWhatsApp}
+  <Features variant="featureWhatsApp" data={featureWhatsApp} />
+{/if}
+{#if featureContact}
+  <Features variant="featureContact" data={featureContact} />
+{/if}
